66fa087f30f67977dfa242618948e96822b53c72
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-24 23:07:30 +0900

    Create SR_Image.js

---
+++ file/SR_Image/SR_Image.js
@@ -0,0 +1,13 @@
+function SR_Image(){ // original name: wa()
+    this.IG_image = 0;           // image                       original name: .Z
+    this.IG_file = "";           // filename                    original name: .file
+    this.IG_font = 0;            // font?                       original name: .ib
+    this.IG_ctx = 0;             // context                     original name: .ca
+    this.IG_canvas = 0;          // canvas                      original name: .Xa
+    this.IG_cropped_width = 0;   // width (without black space) original name: .u
+    this.IG_dimensions = 0;      // dimensions                  original name: .d
+    this.IG_pxl_color_index = 0; // array of each pixel's color original name: .f
+    //this.IG_shift = 0;           //                             original name: .shift
+    this.IG_height = 0;          // height                      original name: .s
+    this.IG_width = 0;           // width                       original name: .i
+}
