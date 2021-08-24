4cf68a1e41c76c9ff78445499b0f90555b0c5441
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-25 07:41:01 +0900

    Create getSaveCode.js

---
+++ file/function/getSaveCode.js
@@ -0,0 +1,6 @@
+window.GameSave = getSaveCode;
+function getSaveCode(save_string_var){ // original name: ie()
+    if (save_string_var.length==0)
+         return "";
+    else return Save_Code3;
+}
