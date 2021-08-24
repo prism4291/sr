61718d384607d2a4a998d08233c91349c20499e4
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-24 23:09:23 +0900

    Create SR_Text.js

---
+++ file/SR_Text/SR_Text.js
@@ -0,0 +1,7 @@
+function SR_Text(){ // original name: Zh()
+    this.TX_image = new SR_Image; // source image for the text original name: .N
+    this.TX_start_pos = 0;        // start position for letter original name: .P
+    this.TX_spacing = 0;          // space between each letter original name: .Q
+    this.TX_height = 0;           // height of each letter     original name: .$a
+    this.TX_width = 0;            // width of each letter      original name: .R
+}
