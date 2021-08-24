32ed941db1f2dea402f5f6d4410af3e721ad2b0c
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-24 23:09:51 +0900

    Create TXset.js

---
+++ file/SR_Text/TXset.js
@@ -0,0 +1,7 @@
+SR_Text.prototype.TXset = function(image,width,height){ // original name: Zh.prototype.j
+    this.TX_image.IGset(image);
+    this.TX_width = width;
+    this.TX_height = height;
+    this.TX_start_pos = 0;
+    this.TX_spacing = 0;
+};
