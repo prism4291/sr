aa721e70f63d7d0e2289b599c405b378c5bc01d6
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-25 19:47:51 +0900

    Create fiftyfifty.js

---
+++ file/function/fiftyfifty.js
@@ -0,0 +1,8 @@
+function fiftyfifty(num1,num2){ // original name: Kf()
+    Rand_EF = Rand_EF+Rand_FF;
+    Rand_EF &= 1023;
+
+    if (Rand_Arr_Df[Rand_EF]<0.5)
+         return num1;
+    else return num2;
+}
