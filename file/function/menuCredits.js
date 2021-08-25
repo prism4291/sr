5d31bb3e2ccee9f1ed3752a5ed44e194c25cc295
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-25 12:38:21 +0900

    Create menuCredits.js

---
+++ file/function/menuCredits.js
@@ -0,0 +1,4 @@
+function menuCredits(){ // original name: Ff()
+    filledRect(0,Win_Height-16,Win_Width,16,0x000000);
+    centeredText(Large_Text,Win_Hcenter,Win_Height-8,"Copyright (C) 2008 ha55ii DAN-BALL.jp",-1,0x663300);
+}
