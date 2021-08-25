52194e36ce380dd6a2537f8693ec6a4bee7f4343
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-25 12:53:32 +0900

    Create dispItemCentered.js

---
+++ file/function/dispItemCentered.js
@@ -0,0 +1,3 @@
+function dispItemCentered(image,x_pos,y_pos,width,height,src_Xpos,src_Ypos,src_width,src_height,color){ // original name: J()
+    dispItem(image,x_pos-(width>>1),y_pos-(height>>1),width,height,src_Xpos,src_Ypos,src_width,src_height,color);
+}
