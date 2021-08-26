fe7ebd19f6a66255be2441569c0453b96dc70119
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-26 16:46:14 +0900

    Update hub.js

--- hub.js
+++ hub.js
@@ -1,3 +1,4 @@
 document.write('<script src="Vanilla Translation Mod.js"></script>');
 document.write('<script type="text/javascript">Init("1",1,0);</script>');
-document.addEventListener('touchmove', function(e) {e.preventDefault();}, {passive: false});
+//document.addEventListener('touchmove', function(e) {e.preventDefault();}, {passive: false});
+document.addEventListener("touchmove", preventScroll, false);
