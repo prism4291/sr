691e8b496b7c11536a71b901785933d7155dafb2
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-27 08:00:26 +0900

    Update Stickman.js

--- file/SR_Player/Stickman.js
+++ file/SR_Player/Stickman.js
@@ -1,7 +1,7 @@
 // empty class (class with weapons unequipped)
 window.fff = SR_Player.prototype.Stickman_pl;
 SR_Player.prototype.Stickman_pl = function(current_char){ // (new prototype)
-    //#*
+    //#*add
     pullJoints(this.PL_joint[current_char][0],this.PL_joint[current_char][1] ,3.6,0.5,0.5); // top of head to neck
     pullJoints(this.PL_joint[current_char][1],this.PL_joint[current_char][2] ,3.6,0.5,0.5); // neck to crotch
     pullJoints(this.PL_joint[current_char][1],this.PL_joint[current_char][3] ,4.8,0.5,0.5); // neck to left elbow
@@ -28,6 +28,7 @@ SR_Player.prototype.Stickman_pl = function(current_char){ // (new prototype)
     pullJoints(this.PL_joint[current_char][7],this.PL_joint[current_char][8] ,60,0.001,0.001); // left knee to right knee
     //*/
   //pullJoints(this.PL_joint[current_char][7],this.PL_joint[current_char][8] , 6,4  ,0.1); // makes them fly (ish)
+    /*add*/
     pullJoints(this.PL_joint[current_char][0],this.PL_joint[current_char][1] ,0.1,0.5,0.5); // top of head to neck
     pullJoints(this.PL_joint[current_char][1],this.PL_joint[current_char][2] ,0.1,0.5,0.5); // neck to crotch
     pullJoints(this.PL_joint[current_char][1],this.PL_joint[current_char][3] ,0.1,0.5,0.5); // neck to left elbow
@@ -39,4 +40,5 @@ SR_Player.prototype.Stickman_pl = function(current_char){ // (new prototype)
     pullJoints(this.PL_joint[current_char][7],this.PL_joint[current_char][9] ,0.1,0.5,0.5); // left knee to left foot
     pullJoints(this.PL_joint[current_char][8],this.PL_joint[current_char][10],0.1,0.5,0.5); // right knee to right foot
     pullJoints(this.PL_joint[current_char][7],this.PL_joint[current_char][8] ,6  ,0.1,0.1); // left knee to right knee
+    //*/
 };
