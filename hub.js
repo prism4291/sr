1ef8c131ddc39c21b94566e04a5899fb87d60348
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-27 07:05:35 +0900

    Update hub.js

--- hub.js
+++ hub.js
@@ -191,6 +191,9 @@ document.write('<script src="file/Vector2D/Vset.js"></script>');
 document.getElementById("cv").style.position = "absolute";
 document.getElementById("cv").style.left = "100px";
 document.getElementById("cv").style.top = "50px";
+document.getElementById("inputBox").style.position = "absolute";
+document.getElementById("inputBox").style.top = "500px";
+
 document.write('<script src="file/var.js"></script>');
 document.write('<script src="file/Stage_Spawns.js"></script>');
 document.write('<script src="file/Shop_Items.js"></script>');
