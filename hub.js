cf2cc26f7d2842f8cd30e1314c260b47c701de05
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-26 16:34:31 +0900

    Update hub.js

--- hub.js
+++ hub.js
@@ -1,2 +1,5 @@
 document.write('<script src="Vanilla Translation Mod.js"></script>');
 document.write('<script type="text/javascript">Init("1",1,0);</script>');
+document.body.addEventListener('touchstart', function(event) {
+  event.preventDefault();
+});
