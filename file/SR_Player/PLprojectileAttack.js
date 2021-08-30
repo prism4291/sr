9ac7bf69cbb27596585d70d4c25a1ee1042aaf2b
prism4291 <74530015+prism4291@users.noreply.github.com>
2021-08-30 18:22:22 +0900

    Update PLprojectileAttack.js

--- file/SR_Player/PLprojectileAttack.js
+++ file/SR_Player/PLprojectileAttack.js
@@ -321,7 +321,7 @@ SR_Player.prototype.PLprojectileAttack = function(source,AT_focus_Xpos,AT_focus_
         }
         if (checkEff(Stickmen_Slots+source,Card_Reflct) && getEff(Stickmen_Slots+source,Eff1)>random(100))
             AT_pj_bounce = 2;
-        /*add*/
+        /*add*#/
         AT_pj_bounce = 2;
         AT_weap_bullet = AT_weap_bullet*100;
         //*/
